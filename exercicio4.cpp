af4779abd502be69e937c5c44122c1b534607d2c
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-25 21:09:46 -0300

    exercicio 4 incompleto

---
+++ exercicio4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <regex>
+
+using namespace std;
+
+int main() {
+    double valor1, valor2, resultado;
+
+    cout << "Digite um valor" << endl;
+    cin >> valor1;
+
+    cout << "Digite o segundo valor" << endl;
+    cin >> valor2; 
+
+    resultado = valor1 + valor2;
+    resultado = (valor1 + valor2) / 2;
+    resultado = valor1 * valor2;
+    resultado = valor1 + valor2;
+
+    
+    return 0;
+}
