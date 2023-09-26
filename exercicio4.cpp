553049ba352b40e4a19ef2ae77dcab1a1c91ee16
mikaelleRubia <mikaelle.rubia@outlook.com>
2023-09-26 08:51:55 -0300

    feture: exercicio 4, da letra A até G

--- exercicio4.cpp
+++ exercicio4.cpp
@@ -14,9 +14,26 @@ int main() {
     cin >> valor2; 
 
     resultado = valor1 + valor2;
+    cout << "Resultado de soma: "<< resultado << endl;
+
     resultado = (valor1 + valor2) / 2;
+    cout << "Resultado da media: "<< resultado << endl;
+
     resultado = valor1 * valor2;
-    resultado = valor1 + valor2;
+    cout << "Resultado do produto: "<< resultado << endl;
+
+
+    if(valor2 == valor1){
+        cout << "Estes nuumeros saao iguais " << endl;
+    }
+    else if(valor1 > valor2){
+        cout << "E maior: " << valor1<< endl;
+        cout << "E menor: " << valor2<< endl;
+    }else if(valor2 > valor1){
+        cout << "E maior: " << valor2 << endl;
+        cout << "E menor: " << valor1 << endl;
+    }    
+
 
     
     return 0;
